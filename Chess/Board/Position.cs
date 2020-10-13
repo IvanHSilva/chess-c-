30d24d42eeece53f610b14c097e14126a336a23a
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-10-13 19:47:17 -0300

    Position class

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess.Board {
+    class Position {
+        public int Row {get; set;}
+        public int Column { get; set; }
+
+        public Position(int row, int column) {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString() {
+            return Row + ", " + Column;
+        }
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Chess.Board;
+using System;
 
 namespace Chess {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Position P = new Position(3, 4);
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
